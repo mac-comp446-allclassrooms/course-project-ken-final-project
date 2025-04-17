6e5b0cb2634ee62168a0b6f528e72d86064547f2
ekarman <ekarman@macalester.edu>
2025-04-17 14:15:27 -0500

    Added character models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Character, DungeonsAndDragonsFifthEditionCharacter
 
 # Register your models here.
+
+admin.site.register(Character)
+admin.site.register(DungeonsAndDragonsFifthEditionCharacter)
--- main/models.py
+++ main/models.py
@@ -2,11 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class UserTemplates(models.Model):
+class UserTemplate(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='usertemplates', null=True)
 	name = models.CharField(max_length=200)
 
 	def __str__(self):
 		return self.name
 
+class Character(models.Model):
+	name = models.CharField(max_length=200)
+	system = models.CharField(max_length=200)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+class DungeonsAndDragonsFifthEditionCharacter(Character):
+	species = models.CharField(max_length=200)
+	characterClass = models.CharField(max_length=200)
