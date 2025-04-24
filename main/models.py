f01f7285f7c7335f2e2896e0a7518240f4d9ad01
ekarman <ekarman@macalester.edu>
2025-04-24 10:41:04 -0500

    Added ability scores, skills, attacks. Menu is now dynamic w/r/t sheet templates

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
+import math
 from django.db import models
 from django.contrib.auth.models import User
 from model_utils.managers import InheritanceManager
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class UserTemplate(models.Model):
@@ -18,10 +20,28 @@ class Character(models.Model):
 class DungeonsAndDragonsFifthEditionCharacter(Character):
 	system = "D&D 5e"
 	species = models.CharField(max_length=200)
-	characterClass = models.CharField(max_length=200)
-	level = models.PositiveIntegerField()
+	character_class = models.CharField(max_length=200)
+	level = models.PositiveIntegerField(default=1)
+
+class DungeonsAndDragonsFifthEditionAbilityScore(models.Model):
+	name = models.CharField(max_length=200)
+	score = models.PositiveIntegerField(default=10)
+	character = models.ForeignKey(DungeonsAndDragonsFifthEditionCharacter, on_delete=models.CASCADE)
+
+class DungeonsAndDragonsFifthEditionSkill(models.Model):
+	name = models.CharField(max_length=200)
+	proficiency = models.PositiveIntegerField(default=0) # 0 for not proficient, 1 if proficient, 2 if expertise, 0.5 if jack of all trades
+	ability_score = models.ForeignKey(DungeonsAndDragonsFifthEditionAbilityScore, on_delete=models.CASCADE)
+	character = models.ForeignKey(DungeonsAndDragonsFifthEditionCharacter, on_delete=models.CASCADE)
 
 class DungeonsAndDragonsFifthEditionItem(models.Model):
 	name = models.CharField(max_length=200)
 	description = models.CharField(max_length=10000)
+	character = models.ForeignKey(DungeonsAndDragonsFifthEditionCharacter, on_delete=models.CASCADE)
+
+class DungeonsAndDragonsFifthEditionAttack(models.Model):
+	name = models.CharField(max_length=200)
+	attack_bonus = models.IntegerField(default=0)
+	damage_amount = models.CharField(max_length=200)
+	damage_type = models.CharField(max_length=200)
 	character = models.ForeignKey(DungeonsAndDragonsFifthEditionCharacter, on_delete=models.CASCADE)
