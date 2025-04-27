5b3328232074d8d6d1329c0fdf6ee1ad14d6681c
Kaliana Andriamananjara <kandriam@macalester.edu>
2025-04-27 01:13:45 -0500

    Weapons, Spells and Items
    Weapon, spell and item block created with create and delete weapon/spell/item functional visually

--- main/static/menu.js
+++ main/static/menu.js
@@ -32,3 +32,80 @@ function createThumbnail(character) {
             + "<button>Open</button>" + "<button>Delete</button>";
     return newThumbnail;
 }
+
+// Creates new weapon row in HTML weapon table
+function createNewWeapon() {
+    const newWeapon = document.createElement("tr");
+    newWeapon.classList.add("weapon");
+
+    newWeapon.innerHTML = "<td> <input type='text' placeholder='Weapon Name'></input></td>"
+            + "<td><input type='text' placeholder='Type'></input></td>"
+            + "<td>" + "<select name='mod'>"
+                    + "<option value='Dexterity'>Dex</option>"
+                    + "<option value='Strength'>Str</option>"
+            +" </select>" + "</td>"
+            + "<td><input type='text' placeholder='Range'></input></td>"
+            + "<td><input type='text' placeholder='Damage Roll'></input></td>"
+            + "<td><input type='text' placeholder='Damage Type'></input></td>"
+            + "<td><button type='button' onclick = 'deleteWeapon(this)'>delete</button></td>"
+
+    console.log("Weapon created");
+    const statblock = document.getElementById("weaponblock");
+    statblock.appendChild(newWeapon);
+}
+
+// Deletes weapon row in HTML weapon table
+function deleteWeapon(loc) {
+    console.log("Weapon Deleted");
+    grandparent = loc.parentNode.parentNode;
+    console.log("Deleted " + grandparent);
+    grandparent.remove();
+}
+
+// Creates new spell row in HTML weapon table
+function createNewSpell() {
+    const newWeapon = document.createElement("tr");
+    newWeapon.classList.add("spell");
+    newWeapon.innerHTML = "<td> <input type='text' placeholder='Spell Name'></input></td>"
+            + "<td><input type='number' value='0'></input></td>"
+            + "<td><input type='text' placeholder='Description'></input></td>"
+            + "<td><input type='text' placeholder='Attack'></input></td>"
+            + "<td><input type='text' placeholder='Save'></input></td>"
+            + "<td><input type='text' placeholder='School'></input></td>"
+            + "<td><button type='button' onclick = 'deleteSpell(this)'>delete</button></td>"
+
+    console.log("spell created");
+    const statblock = document.getElementById("spellblock");
+    statblock.appendChild(newWeapon);
+}
+
+// Deletes spell row in HTML weapon table
+function deleteSpell(loc) {
+    console.log("Spell Deleted");
+    grandparent = loc.parentNode.parentNode;
+    console.log("Deleted " + grandparent);
+    grandparent.remove();
+}
+
+// Creates new spell row in HTML weapon table
+function createNewItem() {
+    const newWeapon = document.createElement("tr");
+    newWeapon.classList.add("item");
+    newWeapon.innerHTML = "<td> <input type='number' value='1'></input></td>"
+            + "<td> <input type='text' placeholder='Item Name'></input></td>"
+            + "<td><input type='text' placeholder='Description'></input></td>"
+            + "<td><input type='text' placeholder='Weight'></input></td>"
+            + "<td><button type='button' onclick = 'deleteItem(this)'>delete</button></td>"
+
+    console.log("spell created");
+    const statblock = document.getElementById("inventoryblock");
+    statblock.appendChild(newWeapon);
+}
+
+// Deletes spell row in HTML weapon table
+function deleteItem(loc) {
+    console.log("Item Deleted");
+    grandparent = loc.parentNode.parentNode;
+    console.log("Deleted " + grandparent);
+    grandparent.remove();
+}
