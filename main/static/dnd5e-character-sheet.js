63a55e6a5a7b68065eed36fca3ba37df9ce319b0
Kaliana Andriamananjara <kandriam@macalester.edu>
2025-04-30 12:52:46 -0500

    Weapons are just attack now

--- main/static/dnd5e-character-sheet.js
+++ main/static/dnd5e-character-sheet.js
@@ -1,27 +1,24 @@
 
 // Creates new weapon row in HTML weapon table
-function createNewWeapon() {
-    const newWeapon = document.createElement("tr");
-    newWeapon.classList.add("weapon");
+function createNewAttack() {
+    const newAttack = document.createElement("tr");
+    newAttack.classList.add("attack");
 
-    newWeapon.innerHTML = "<td> <input type='text' name='name_weapon' placeholder='Weapon Name'></input></td>"
-            + "<td><input type='text' name='type_weapon' placeholder='Type'></input></td>"
-            + "<td>" + "<select name='ability_score_weapon'>"
-                    + "<option value='Dexterity'>Dex</option>"
-                    + "<option value='Strength'>Str</option>"
-            +" </select>" + "</td>"
-            + "<td><input type='text' name='range_weapon' placeholder='Range'></input></td>"
-            + "<td><input type='text' name='damage_roll_weapon' placeholder='Damage Roll'></input></td>"
-            + "<td><input type='text' name='damage_type_weapon' placeholder='Damage Type'></input></td>"
-            + "<td><button type='button' onclick = 'deleteWeapon(this)'>delete</button></td>"
+    newAttack.innerHTML = "<td> <input type='text' name='name_attack' placeholder='Name'></input></td>"
+            + "<td><input type='text' name='range_attack' placeholder='Range'></input></td>"
+            + "<td><input type='text' name='damage_roll_attack' placeholder='Damage Roll'></input></td>"
+            + "<td><input type='text' name='bonus_attack' placeholder='Bonus'</td>"
+            + "<td><input type='text' name='damage_type_attack' placeholder='Damage Type'></input></td>"
+            + "<td><input type='text' name='notes_attack' placeholder='Notes'></input></td>"
+            + "<td><button type='button' onclick = 'deleteAttack(this)'>Delete</button></td>"
 
     console.log("Weapon created");
-    const statblock = document.getElementById("weaponblock");
-    statblock.appendChild(newWeapon);
+    const statblock = document.getElementById("attackblock");
+    statblock.appendChild(newAttack);
 }
 
 // Deletes weapon row in HTML weapon table
-function deleteWeapon(loc) {
+function deleteAttack(loc) {
     console.log("Weapon Deleted");
     grandparent = loc.parentNode.parentNode;
     console.log("Deleted " + grandparent);
@@ -38,7 +35,7 @@ function createNewSpell() {
             + "<td><input type='text' placeholder='Attack'></input></td>"
             + "<td><input type='text' placeholder='Save'></input></td>"
             + "<td><input type='text' placeholder='School'></input></td>"
-            + "<td><button type='button' onclick = 'deleteSpell(this)'>delete</button></td>"
+            + "<td><button type='button' onclick = 'deleteSpell(this)'>Delete</button></td>"
 
     console.log("spell created");
     const statblock = document.getElementById("spellblock");
@@ -61,7 +58,7 @@ function createNewItem() {
             + "<td> <input type='text' placeholder='Item Name'></input></td>"
             + "<td><input type='text' placeholder='Description'></input></td>"
             + "<td><input type='text' placeholder='Weight'></input></td>"
-            + "<td><button type='button' onclick = 'deleteItem(this)'>delete</button></td>"
+            + "<td><button type='button' onclick = 'deleteItem(this)'>Delete</button></td>"
 
     console.log("spell created");
     const statblock = document.getElementById("inventoryblock");
